ac6c162bf97472e9ff2c83d5b373fc0914e2b1fc
lucky masharane <lmasharane@gmail.com>
2021-01-25 13:47:53 +0200

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCe3E7uq3KRtllxgL9ivtFRUn-fw3uubSI",
+    authDomain: "job-app-93360.firebaseapp.com",
+    projectId: "job-app-93360",
+    storageBucket: "job-app-93360.appspot.com",
+    messagingSenderId: "59184044214",
+    appId: "1:59184044214:web:6cd16c9264a5ccaf4cee17"
+  }
 };
